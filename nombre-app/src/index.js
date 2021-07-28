0a3c28bf7fff1dc48d9f747bdb4dd01ba46b69aa
Mariela Santa Cruz <mariela165santacruz@gmail.com>
2021-07-28 15:24:17 -0300

    creacion del respositorio

---
+++ nombre-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+reactDom.render(
+  <div>
+    WingSports
+      <ul>
+        <li>Inicio</li>
+        <li>Categorias</li>
+        <li>Productos</li>
+        <li>Carrito</li>
+      </ul>
+  </div>,
+  document.getElementById("root")
+);
