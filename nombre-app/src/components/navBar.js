79dceae25bf7724f8d6be298ac3e972f2b7426ef
Mariela Santa Cruz <mariela165santacruz@gmail.com>
2021-08-30 11:41:31 -0300

    agregado de productos

--- nombre-app/src/components/navBar.js
+++ nombre-app/src/components/navBar.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import "../App.css";
 import "./Navbar.css";
-import logo from "./img/logo.png";
+import logo from "./img/logos.png";
 import CartWidget from "./CartWidget";
 import { Link } from "react-router-dom";
 
@@ -32,7 +32,7 @@ function NavBar() {
                 width="40px"
                 height="40px"
               />
-              <h1 className="d-inline fs-4 align-middle px-3">EdTech.js</h1>
+              <h1 className="d-inline fs-4 align-middle px-3">Gol de oro</h1>
             </Nav.Link>
           </Navbar.Brand>
 
@@ -40,18 +40,18 @@ function NavBar() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto ">
               <NavDropdown title="Categorías" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/category/libros">
-                  Libros
+                <NavDropdown.Item as={Link} to="/category/camisetas">
+                  Camisetas
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/category/pdf">
-                  PDF
+                <NavDropdown.Item as={Link} to="/category/short">
+                  Short
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/category/videos">
-                  Videos
+                <NavDropdown.Item as={Link} to="/category/medias">
+                  Medias
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="/category/cursos">
-                  Cursos
+                <NavDropdown.Item as={Link} to="/category/pelota">
+                  Pelota
                 </NavDropdown.Item>
               </NavDropdown>
               <Nav.Link as={Link} to="/Nosotros">
